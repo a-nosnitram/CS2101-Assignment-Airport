02772b34c58c9b6328484ac516ef784973fc6919
a-nosnitram <stasymartinson@gmail.com>
2024-10-22 20:02:05 +0200

    Added design folder

---
+++ design/Colours.java
@@ -0,0 +1,16 @@
+package design;
+
+public class Colours {
+    // reset code
+    public static final String RESET = "\033[0m";
+
+    // text color codes
+    public static final String RED = "\033[31m";
+    public static final String GREEN = "\033[32m";
+    public static final String YELLOW = "\033[33m";
+    public static final String BLUE = "\033[34m";
+    public static final String BOLD = "\033[1m";
+    public static final String ITALICS = "\033[3m";
+    public static final String GRAY = "\033[90m";
+    public static final String PURPLE = "\u001B[35m";
+}
